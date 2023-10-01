4fcd0a6c4a2bb58a36ddbca8f6bb64a7e42534da
Isak PH <isakhaer@gmail.com>
2023-10-01 12:51:36 +0200

    still bugs while holding conversations

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gpt
 
-A small program to request answers from a ChatGPT model, either 3.5 or 4. Which can be specified as "model=3.5" or "model=4" from the command line.
+A small program to request answers from a ChatGPT model through OpenAI's API, either 3.5 or 4. Which can be specified as "model=3.5" or "model=4" from the command line.
+
+A personal key from OpenAI is necessary to run the program.
 
 Add to path to run from the command line.
--- app/gpt.py
+++ app/gpt.py
@@ -1,33 +1,9 @@
 import os
-import argparse
 import openai
-from typing import Union
-from openai.openai_object import OpenAIObject
+from typing import Union, Dict, List
+# from openai.openai_object import OpenAIObject
 
 openai.api_key = os.environ.get('OPENAI_KEY')
-
-# Think I'll skip implementing something like this. Just assume 3.5, as it is a lot cheaper.
-# def choose_model(answer: Union[3.5, 4]) -> str:
-#     if answer == 3.5:
-#         return "gpt-3.5-turbo"
-#     return "gpt-4"
-
-def choose_model(model: Union[int, float]) -> str:
-    """
-    Accepts the values 3.5 and 4 and returns either the string 'gpt-3.5-turbo' or 'gpt-4'.
-    """
-    assert model == 3.5 or model == 4
-    # while True:
-    #     try: 
-    #         choice = int(input("To choose model, enter 3.5 or 4: "))
-    #     except ValueError:
-    #         print("Invalid input. Try again.")
-    #         continue
-    #     else: break
-
-    if model == 3.5:
-        return "gpt-3.5-turbo"
-    return "gpt-4"
 
 
 class GPT_conversation:
@@ -37,79 +13,96 @@ class GPT_conversation:
     used to carry on a conversation indefinitely or start new conversations
     as desired by the user. 
     """
-    def __init__(self, model: str):
-        assert model == "gpt-3.5-turbo" or model == "gpt-4"
+    def __init__(self, options: Dict[str, Union[int, float]], stream: bool = False):
         """
         Initiates a GPT_conversation object with default values.
+        @param options: At the moment, this only implements model choice: the user
+        can choose between 'gpt-3.5-turbo' by entering 3.5 and 'gpt-4' by entering 4.
+        @param stream: I intend to let the user enable stream at some point.
         """
-        self.messages = [] 
+        self._model = self.__choose_model(options['model'])
 
 
-def get_first_response(message: str = None, print_to_terminal: bool = True) -> OpenAIObject:
-    """
-    Asks the user to enter:
+    def __choose_model(self, choice: Union[int, float] = 3.5) -> str:
+        """
+        Accepts 3.5 or 4 as choice and returns either 'gpt-3.5-turbo' or 'gpt-4'.
+        3.5 is the default value simply because it is cheaper to run.
+        """
+        if choice == 4:
+            return 'gpt-4'
+        return 'gpt-3.5-turbo'
 
-    1) which model should be used, # TODO: Make a coherent implementation of model choice.
-    2) a prompt, 
 
-    and then requests a chat completion. The message is printed
-    and the chat completion object returned.
-    Might add support for other things than writing the message to stdout later.
-    """
-    if message is None:
-        message = input("Your prompt: ")
-    response = openai.ChatCompletion.create(
-        #model = choose_model(int(input("enter 3.5 or 4"))),
-        model = "gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message}
-            ]
-        )
-    
-    if print_to_terminal:
-        print(response['choices'][0]['message']['content'])
-    return response
-    
-def hold_conversation(first_response: OpenAIObject) -> None:
-    """
-    Lets the user keep a conversation with ChatGPT (always 3.5 for the time being).
-    The conversation keeps going until the user types '0'.
-    """
-    
-    convo = [first_response['choices'][0]]
-    keep_going = True
-    while keep_going:
-        next_message = input("Type your next prompt (or: 0 to stop, 1 to start a new conversation): ")
-        try:
-            if int(next_message) == 0:
-                break
-            if int(next_message) == 1:
-                ... # TODO: Implement starting a new conversation
-                # convo = something
-        except ValueError:
-            pass # Anything other than 0 is used to create a new chat completion.
+    def __get_first_response(self, 
+                        message: str = None,
+                        print_to_terminal: bool = True # might add some other way of doing this
+                        ) -> List[Dict[str, str]]:
+        """
+        Asks the user to enter a prompt during the creation of the ChatCompletion. 
+        The response to this prompt is printed to terminal.
+        """
+        if message is None:
+            message = input("Your prompt: ")
+        # if options is not None: # this should be deletable
+        #     assert options['model'] == 3.5 or options['model'] == 4
+        #     model_choice = self.__choose_model(options['model'])
+
+        chat_completion = openai.ChatCompletion.create(
+            model = self._model,
+            messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message}
+                ]
+            )
         
-        convo.append({"role": "user", "content": next_message})
-
-        response = openai.ChatCompletion.create(
-        # model = choose_model(int(input("enter 3.5 or 4"))),
-            model = "gpt-3.5-turbo",
-            messages = convo # TODO :I pass something wrong along somewhere. It's probably from when I created convo object originally.:
-            # This was the error: openai.error.InvalidRequestError: Additional properties are not allowed ('finish_reason', 'index', 'message' were unexpected) - 'messages.0'
-        )
-
-        print(response['choices'][0]['message']['content']) # this might have to be tweaked as well
-        convo.append(response['choices'][0])
+        if print_to_terminal:
+            print(chat_completion['choices'][0]['message']['content'])
+        return [{"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message},
+                {'content': chat_completion['choices'][0]['message']['content'],
+                 'user': 'assistant'}]
         
 
-def main():
-    first_response = get_first_response()
-    hold_conversation(first_response)
+    def hold_conversation(self) -> None:
+        """
+        Lets the user keep a conversation with the chose model.
+        The conversation keeps going until the user types '0'.
+        The user can press 1 to initiate a new conversation
+        """
+        
+        # convo = [first_response['choices'][0]]
+        convo = self.__get_first_response()
+        
+        keep_going = True
+        while keep_going:
+            next_message = input("Type your next prompt (0 to stop, 1 to start a new conversation): ")
+            try:
+                if int(next_message) == 0:
+                    break
+                if int(next_message) == 1:
+                    convo = self.__get_first_response()
+                    continue
+            except ValueError:
+                pass # Anything other than 0 or 1 is used to create a new chat completion.
+            
+            convo.append({"content": next_message, "role": "user"})
+            print("taking a look at 'convo' before creating a second response:\n", convo)
 
-# response = get_first_response()
-# print(type(response))
-# print(response['choices'][0]['message']['content'])
+            response = openai.ChatCompletion.create(
+                model = self._model,
+                messages = convo # TODO :I pass something wrong along somewhere. It's probably from when I created convo object originally.:
+                # This was the error: openai.error.InvalidRequestError: Additional properties are not allowed ('finish_reason', 'index', 'message' were unexpected) - 'messages.0'
+            )
+
+            print(response['choices'][0]['message']['content']) # this might have to be tweaked as well
+            convo.append(response['choices'][0])
+        
+
+def main(options = None):
+    gpt_model = GPT_conversation(options)
+    gpt_model.hold_conversation()
+
 
 if __name__ == "__main__":
-    main()
+    options = {'model': 3.5}
+    main(options)
